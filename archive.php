1d1ad79a98516564205583e76a4381feb283d6c4
Bradley-D <Bradley-D@users.noreply.github.com>
2019-04-11 10:44:58 +1000

    [ ADDED ] basic 60 40 layout to archive

--- archive.php
+++ archive.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="col-sm-12 col-md-8 content-area">
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
